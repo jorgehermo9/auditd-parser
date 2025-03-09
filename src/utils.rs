ac0795f49a2ba9a62aea75dac414cd38607af459
Jorge Hermo <jorgehermogonzalez@gmail.com>
2025-03-09 17:21:35 +0100

    fix: clippy lints

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,7 @@ use nom::IResult;
 /// Parser that consumes all of its input and returns it as-is.
 ///
 /// This funcion always succeeds.
+#[allow(clippy::unnecessary_wraps)]
 pub fn burp(input: &str) -> IResult<&str, &str> {
     Ok(("", input))
 }
