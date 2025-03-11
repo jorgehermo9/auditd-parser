3d22ea74feb8d09a9f757505ab664d648a928deb
Jorge Hermo <jorgehermogonzalez@gmail.com>
2025-03-12 00:26:25 +0100

    feat: refactor parse string value

--- src/parser/body/value.rs
+++ src/parser/body/value.rs
@@ -2,8 +2,8 @@ use crate::FieldValue;
 use crate::utils::burp;
 use nom::AsChar;
 use nom::branch::alt;
-use nom::bytes::complete::{tag, take_until1, take_while1};
-use nom::character::complete::u64 as parse_u64;
+use nom::bytes::complete::{tag, take_until1, take_while, take_while1};
+use nom::character::complete::{char, u64 as parse_u64};
 use nom::combinator::all_consuming;
 use nom::sequence::delimited;
 use nom::{IResult, Parser};
@@ -14,9 +14,22 @@ use super::{ENRICHMENT_SEPARATOR, parse_key_value_list};
 
 /// Parses a string value, which can be surrounded by single or double quotes.
 fn parse_string_value(input: &str) -> IResult<&str, &str> {
+    const DOUBLE_QUOTE: char = '"';
+    const SINGLE_QUOTE: char = '\'';
+
+    // TODO: handle scaped double quote inside double quoted string with `scaped` parser
+    // TODO: handle scaped single quote inside single quoted string with `scaped` parser
     alt((
-        delimited(tag("\""), take_until1("\""), tag("\"")),
-        delimited(tag("'"), take_until1("'"), tag("'")),
+        delimited(
+            char(DOUBLE_QUOTE),
+            take_while(|c| c != DOUBLE_QUOTE),
+            char(DOUBLE_QUOTE),
+        ),
+        delimited(
+            char(SINGLE_QUOTE),
+            take_while(|c| c != SINGLE_QUOTE),
+            char(SINGLE_QUOTE),
+        ),
     ))
     .parse(input)
 }
@@ -63,13 +76,28 @@ pub fn parse_value(input: &str) -> IResult<&str, FieldValue> {
     alt((parse_quoted_value, parse_unquoted_value)).parse(input)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use rstest::rstest;
-//     #[rstest]
-//     fn test_parse_string_value(#[case] input: &str, #[case] expected: &str) {
-//         let (_, result) = parse_string_value(input).unwrap();
-//         assert_eq!(result, expected);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+    #[rstest]
+    #[case::double_quoted("\"foo\"", "foo")]
+    #[case::single_quoted("'foo'", "foo")]
+    #[case::double_quoted_with_space("\"foo bar\"", "foo bar")]
+    #[case::single_quoted_with_space("'foo bar'", "foo bar")]
+    #[case::double_quoted_empty_string("\"\"", "")]
+    #[case::single_quoted_empty_string("''", "")]
+    fn test_parse_string_value(#[case] input: &str, #[case] expected: &str) {
+        let (remaining, result) = parse_string_value(input).unwrap();
+        assert!(remaining.is_empty());
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
+    #[case::unquoted("foo")]
+    #[case::unquoted_with_space("foo bar")]
+    #[case::mixed_quotes("\"foo'")]
+    fn test_parse_string_value_fails(#[case] input: &str) {
+        assert!(parse_string_value(input).is_err());
+    }
+}
